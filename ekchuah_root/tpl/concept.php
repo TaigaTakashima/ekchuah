54b81dcf52141a4b43eca335bb66191ae8065224
高島大河 <osakahal90557@gmail.com>
2021-02-09 17:04:36 +0900

    ver1.2
    css fix

--- ekchuah_root/tpl/concept.php
+++ ekchuah_root/tpl/concept.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="tpl/css/concept.css">
     <link rel="stylesheet" href="tpl/css/header.css">
     <link rel="stylesheet" href="tpl/css/footer.css">
-
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+    <script src="tpl/js/jQuery-3.4.1.min.js"></script>
     <title>Ek Chuah　エクチュア からほり「蔵」本店</title>
 </head>
 <body>
@@ -91,5 +92,175 @@
     </footer>
 
     </div><!--wrapper-->
+    <script>//スクロール
+        var _window = $(window),
+    _header = $('.gnav'),
+    heroBottom;
+
+_window.on('scroll',function(){
+    heroBottom = $('.hero').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('fixed');
+    }
+    else{
+        _header.removeClass('fixed');
+    }
+});
+
+_window.trigger('scroll');
+
+//gnavi
+$('.menu').on('click',function(){
+    $('.menu__line').toggleClass('active');
+    $('.gnav').fadeToggle();
+});
+    </script>
+    
+    <script>
+    $(function(){
+        let setElm = $('.loopSlider'),
+        moveInterval = 3,
+        slideTimeBase = 10,
+        spWidth = 768,
+        spSpeed = 3;
+     
+        $(window).on('load', function(){
+            setElm.each(function(){
+                let self = $(this),
+                selfWidth = self.innerWidth(),
+                findUl = self.find('ul'),
+                findLi = findUl.find('li'),
+                findLink = findLi.find('a'),
+                listCount = findLi.length;
+     
+                listWidth = findLi.outerWidth();
+                loopWidth = listWidth * listCount;
+     
+                findUl.wrapAll('<div class="loopSliderWrap" />');
+                let selfWrap = self.find('.loopSliderWrap');
+     
+                setElm.css({visibility:'visible',opacity:'0'}).animate({opacity:'1'},500);
+     
+                if(loopWidth > selfWidth){
+                    findUl.css({width:loopWidth}).clone().appendTo(selfWrap).clone().prependTo(selfWrap);
+     
+                    selfWrap.css({left:'-' + loopWidth + 'px'});
+     
+                    setSlideTime();
+                    timerLeft();
+     
+                    $(window).on('resize', function(){
+                        clearInterval(setTimer);
+                        setSlideTime();
+                        timerLeft();
+                    }).resize();
+     
+                    function setSlideTime() {
+                        if(window.innerWidth > spWidth){
+                            slideTime = slideTimeBase;
+                        } else {
+                            slideTime = slideTimeBase*spSpeed;
+                        }
+                    }
+     
+                    function timerLeft(){
+                        setTimer = setInterval(function(){loopPositionLeft()},slideTime);
+                    };
+     
+                    function loopPositionLeft(){
+                        listWidth = findLi.outerWidth();
+                        loopWidth = listWidth * listCount;
+     
+                        self.find('ul').css({width:loopWidth});
+                        selfWrap.css({width:loopWidth*3}).stop().animate({left:'-=' + (moveInterval) + 'px'},slideTime,'linear',function(){
+                            let posLeft = parseInt(selfWrap.css('left')),
+                            widthCal = (loopWidth)-((loopWidth)*3);
+                            if (posLeft <= widthCal) {
+                                let calCat = posLeft - widthCal;
+                                //alert(calCat)
+                                selfWrap.css({left:'-' + (loopWidth - calCat) + 'px'});
+                            }
+                        });
+                        return false;
+                    };
+     
+                    function myHandler(e){
+                        e.preventDefault();
+                    }
+     
+                    let isTouch = ('ontouchstart' in window),
+                    ua = navigator.userAgent;
+     
+                    selfWrap.on({
+                        'touchstart mousedown': function(e){
+                            if(!(ua.search(/iPhone/) != -1 || ua.search(/iPad/) != -1 || ua.search(/Macintosh/) != -1 && 'ontouchend' in document || ua.search(/iPod/) != -1 || ua.search(/Android/) != -1)){
+                                e.preventDefault();
+                                $(this).find('a').off('click', myHandler);
+                            }
+                            if(selfWrap.is(':animated')){
+                                clearInterval(setTimer);
+                            }
+                            this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+                            this.leftBegin = parseInt($(this).css('left'));
+                            this.left = parseInt($(this).css('left'));
+                            this.touched = true;
+                        },
+                        'touchmove mousemove': function(e){
+                            if(!this.touched){return;}
+                            if(!(ua.search(/iPhone/) != -1 || ua.search(/iPad/) != -1 || ua.search(/Macintosh/) != -1 && 'ontouchend' in document || ua.search(/iPod/) != -1 || ua.search(/Android/) != -1)){
+                                e.preventDefault();
+                                $(this).find('a').on('click', myHandler);
+                            }
+                            if(selfWrap.is(':animated')){
+                                clearInterval(setTimer);
+                            }
+                            this.left = this.left - (this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX) );
+                            this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+                            $(this).css({left:this.left});
+                        },
+                        'touchend mouseup mouseout': function(e){
+                            if (!this.touched) {return;}
+                            this.touched = false;
+     
+                            outLeft = parseInt($(this).css('left'));
+                            if(outLeft < (loopWidth)-((loopWidth)*3)){
+                                $(this).css({left: outLeft - (loopWidth-(loopWidth*2)) + 'px'});
+                            }
+                            if(outLeft > (loopWidth)-((loopWidth)*2)){
+                                $(this).css({left: outLeft + (loopWidth-(loopWidth*2)) + 'px'});
+                            }
+     
+                            timerLeft();
+                            return false;
+                        }
+                    });
+     
+                    if(ua.search(/iPhone/) != -1 || ua.search(/iPad/) != -1 || ua.search(/Macintosh/) != -1 && 'ontouchend' in document || ua.search(/iPod/) != -1 || ua.search(/Android/) != -1){
+                        selfWrap.find('a').on({
+                            'touchstart': function(e){
+                                thisHref = $(this).attr('href');
+                                thisTarget = $(this).attr('target');
+                                touchFlag = true;
+                            },
+                            'touchmove': function(e){
+                                touchFlag = false;
+                                e.preventDefault();
+                            },
+                            'touchend': function(e){
+                                if(touchFlag == true){
+                                    if(thisTarget == '_blank'){
+                                        window.open(thisHref, '_blank');
+                                    } else {
+                                        location.href = thisHref;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        });
+    });
+    </script>
 </body>
 </html>
