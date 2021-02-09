ccfccf8c614baf68a74ad15f5104b8aab4797a47
Miyagi Koushirou <kou0408@icloud.com>
2021-02-09 11:12:43 +0900

    ver.1.2
    お知らせの画像変更

--- ekchuah_root/tpl/detail.php
+++ ekchuah_root/tpl/detail.php
@@ -31,8 +31,8 @@
             <a href="news.php" class="prev">戻る<img src="tpl/img/news_prev_icon.png" alt=""></a>
             <?php foreach ($news_list as $item):?>
             <h3><?php echo $item['title'];?></h3>
+            <img src="../ekchuah_cms/news/images/<?php echo $item['news_id'];?>.jpg" alt="<?php echo $item['title'];?>" class="sam">
             <?php endforeach;?>
-            <img src="tpl/img/menu_firstview.png" alt="チョコミントパフェ" class="sam">
         </section>
         <section id="sentence">
             <?php foreach ($news_list as $item):?>
